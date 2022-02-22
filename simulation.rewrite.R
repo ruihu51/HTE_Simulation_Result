f7d5170a7bf0cbfdaadaa7862eda7e985ab34640
ruihu51 <ruihu.cufe@outlook.com>
2022-02-22 15:18:48 -0500

    update1

--- simulation.rewrite.R
+++ simulation.rewrite.R
@@ -16,18 +16,23 @@ n <- 2000
 pi0 <- function(w) expit(0.5+(w[,1]/3))
 # mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*w[,1] - 0.3*w[,2] + 0.1*w[,3]
 # mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*w[,1]^2 - 0.3*w[,2] + 0.1*w[,3]
-mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*a*w[,1] - 0.3*w[,2] + 0.1*w[,3]
+# mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*a*w[,1] - 0.3*w[,2] + 0.1*w[,3]
+mu0 <- function(a, w, beta) 0.1 + 0.25*a + 0.75*a*(w[,1]^2+w[,3]) + w[,1] + w[,2]^2
 
 set.seed(95359812)
-W <- data.frame(W1=runif(n, -1, 1), W2=runif(n, -1, 1), W3=runif(n, -1, 1))
+# W <- data.frame(W1=runif(n, -1, 1), W2=runif(n, -1, 1), W3=runif(n, -1, 1))
+W <- data.frame(W1=runif(n, -1, 1), W2=runif(n, -1, 1), W3=rbinom(n, size=1, prob=0.5))
 A <- rbinom(n, size=1, prob=pi0(W))
 Y <- rnorm(n, mean=mu0(A, W), sd=1)
 
-# psi0 <- 0.04
-# theta0 <- 0
+psi0 <- 0.04
+theta0 <- 0
 
 psi0 <- 0.2^2 + (0.5^2)/3
 theta0 <- (0.5^2)/3
+
+psi0 <- (0.25+0.75*(5/6))^2+(0.75^2*(1/5-1/9))+0.75^2*1/4
+theta0 <- (0.75^2*(1/5-1/9))+0.75^2*1/4
 
 # quick check true psi and theta
 # psi0 <- c()
@@ -67,32 +72,39 @@ prop.reg <- SuperLearner(Y=A, X = data.frame(W),
 pi.hat <- prop.reg$SL.predict
 # expit(prop.reg$coefficients[1] + prop.reg$coefficients[2]*W[1,1] +
 #   prop.reg$coefficients[3]*W[1,2] + prop.reg$coefficients[4]*W[1,3])
+
 # 2) estimated outcome regression
 # a) estimate mu1 and mu0 in one model
 ## a.1 glm
 AW <- cbind(data.frame(Y=Y, A=A), data.frame(W))
 
 mu.reg <- glm(Y ~ ., data=AW, family='gaussian')
-mu.reg <- glm(Y ~ A + A*W1 + W2 + W3, data=AW, family='gaussian')
+# mu.reg <- glm(Y ~ A + A*W1 + W2 + W3, data=AW, family='gaussian')
 mu.reg <- glm(Y ~ .^2, data = AW, family = 'gaussian')
 #mu.reg <-  lm(Y ~ A + W)
 # mu.hat <- mu.reg$fitted.values
+# gam.model <- as.formula("Y ~ s(W1, 2) + s(W2, 2) + s(W1):A + s(W2):A + A*W3")
+# mu.reg <- gam::gam(gam.model, data = AW, family = 'gaussian', control = gam::gam.control(maxit = 50, bf.maxit = 50))
+mu.reg <- earth::earth(x = data.frame(cbind(A, W)), y = Y, degree = 2, penalty = -1)
 mu1.hat <- predict(mu.reg, newdata = cbind(data.frame(A = 1), data.frame(W)), type = 'response')
 mu0.hat <- predict(mu.reg, newdata = cbind(data.frame(A = 0), data.frame(W)), type = 'response')
 mu.hats <- data.frame(mu1=mu1.hat, mu0=mu0.hat)
 
 ## a.2 superlearner
-# mu.reg <- SuperLearner(Y=Y, X = data.frame(cbind(A, W)),
-#                        newX = rbind(data.frame(cbind(A=1, W)), data.frame(cbind(A=0, W))),
-#                        SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
-#                        family = 'gaussian',
-#                        obsWeights=rep(1,n),
-#                        id=1:n)
-# mu.hats <- data.frame(mu1=mu.reg$SL.predict[1:n], mu0=mu.reg$SL.predict[-(1:n)])
+learners = create.Learner("SL.earth", params = list(penalty=-1))
+mu.reg <- SuperLearner(Y=Y, X = data.frame(cbind(A, W)),
+                       newX = rbind(data.frame(cbind(A=1, W)), data.frame(cbind(A=0, W))),
+                       # SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
+                       SL.library = c("SL.glm.interaction", learners$names),
+                       family = 'gaussian',
+                       obsWeights=rep(1,n),
+                       id=1:n)
+mu.hats <- data.frame(mu1=mu.reg$SL.predict[1:n], mu0=mu.reg$SL.predict[-(1:n)])
 mu.hat <- A * mu.hats$mu1 + (1-A) * mu.hats$mu0
 
 tau.hat <- mu.hats$mu1 - mu.hats$mu0
 Z.hat <- (2*A - 1) / (A * pi.hat + (1-A) * (1-pi.hat))
+var(tau.hat)
 
 psi.plug.in <- mean(tau.hat^2)
 psi.eif.hat <- 2 * tau.hat * (Y - mu.hat) * Z.hat + tau.hat^2 - psi.plug.in
@@ -117,21 +129,22 @@ mu0.reg  <- glm(Y ~ ., data=AW0, family='gaussian')
 mu0.hat2 <- predict(mu0.reg, newdata = data.frame(W), type = 'response')
 
 ## b.2 superlearner
-# mu0.reg <- SuperLearner(Y=Y[which(A==0)], X = data.frame(W[which(A==0),]),
-#                         newX = data.frame(W),
-#                         SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
-#                         family = 'gaussian',
-#                         obsWeights=rep(1,length(which(A==0))),
-#                         id=1:length(which(A==0)))
-# mu0.hat2 <- mu0.reg$SL.predict
-# mu1.reg <- SuperLearner(Y=Y[which(A==1)], X = data.frame(W[which(A==1),]),
-#                         newX = data.frame(W),
-#                         SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
-#                         # SL.library = c("SL.gam"),
-#                         family = 'gaussian',
-#                         obsWeights=rep(1,length(which(A==1))),
-#                         id=1:length(which(A==1)))
-# mu1.hat2 <- mu1.reg$SL.predict
+mu0.reg <- SuperLearner(Y=Y[which(A==0)], X = data.frame(W[which(A==0),]),
+                        newX = data.frame(W),
+                        # SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
+                        SL.library = c("SL.glm", "SL.gam"),
+                        family = 'gaussian',
+                        obsWeights=rep(1,length(which(A==0))),
+                        id=1:length(which(A==0)))
+mu0.hat2 <- mu0.reg$SL.predict
+mu1.reg <- SuperLearner(Y=Y[which(A==1)], X = data.frame(W[which(A==1),]),
+                        newX = data.frame(W),
+                        # SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
+                        SL.library = c("SL.gam", "SL.glm"),
+                        family = 'gaussian',
+                        obsWeights=rep(1,length(which(A==1))),
+                        id=1:length(which(A==1)))
+mu1.hat2 <- mu1.reg$SL.predict
 
 mu.hat2 <- A * mu1.hat2 + (1-A) * mu0.hat2
 tau.hat2 <- mu1.hat2 - mu0.hat2
@@ -156,6 +169,7 @@ theta.est2 <- ifelse(theta.one.step.est2>0, theta.one.step.est2, theta.plug.in2)
 ##############
 # simulation
 #############
+load("est.sim.rst.redo1.RData")
 seed.fixed <- est.sim.rst.redo1$seed
 seed.list <- c()
 psi.one.step.est.list <- c()
@@ -190,17 +204,22 @@ for (j in 1:1000){
   pi0 <- function(w) expit(0.5+(w[,1]/3))
   # mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*w[,1] - 0.3*w[,2] + 0.1*w[,3]
   # mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*w[,1]^2 - 0.3*w[,2] + 0.1*w[,3]
-  mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*a*w[,1] - 0.3*w[,2] + 0.1*w[,3]
+  # mu0 <- function(a, w) 0.1 + 0.2*a + 0.5*a*w[,1] - 0.3*w[,2] + 0.1*w[,3]
+  mu0 <- function(a, w) 0.1 + 0.25*a + 0.75*a*(w[,1]^2+w[,3]) + w[,1] + w[,2]^2
 
-  W <- data.frame(W1=runif(n, -1, 1), W2=runif(n, -1, 1), W3=runif(n, -1, 1))
+  # W <- data.frame(W1=runif(n, -1, 1), W2=runif(n, -1, 1), W3=runif(n, -1, 1))
+  W <- data.frame(W1=runif(n, -1, 1), W2=runif(n, -1, 1), W3=rbinom(n, size=1, prob=0.5))
   A <- rbinom(n, size=1, prob=pi0(W))
   Y <- rnorm(n, mean=mu0(A, W), sd=1)
 
   # psi0 <- 0.04
   # theta0 <- 0
 
-  psi0 <- 0.2^2 + (0.5^2)/3
-  theta0 <- (0.5^2)/3
+  # psi0 <- 0.2^2 + (0.5^2)/3
+  # theta0 <- (0.5^2)/3
+
+  psi0 <- (0.25+0.75*(5/6))^2+(0.75^2*(1/5-1/9))+0.75^2*1/4
+  theta0 <- (0.75^2*(1/5-1/9))+0.75^2*1/4
 
   ############
   # estimate
@@ -235,13 +254,14 @@ for (j in 1:1000){
   # mu.hats <- data.frame(mu1=mu1.hat, mu0=mu0.hat)
 
   ## a.2 superlearner
+  learners = create.Learner("SL.earth", params = list(penalty=-1))
   mu.reg <- SuperLearner(Y=Y, X = data.frame(cbind(A, W)),
-                         newX = rbind(data.frame(cbind(A=1, W)), data.frame(cbind(A=0, W))),
-                         # SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
-                         SL.library = c("SL.glm.interaction"),
-                         family = 'gaussian',
-                         obsWeights=rep(1,n),
-                         id=1:n)
+                        newX = rbind(data.frame(cbind(A=1, W)), data.frame(cbind(A=0, W))),
+                        # SL.library = c("SL.mean", "SL.glm", "SL.gam", "SL.earth"),
+                        SL.library = c("SL.glm.interaction", learners$names),
+                        family = 'gaussian',
+                        obsWeights=rep(1,n),
+                        id=1:n)
   mu.hats <- data.frame(mu1=mu.reg$SL.predict[1:n], mu0=mu.reg$SL.predict[-(1:n)])
   mu.hat <- A * mu.hats$mu1 + (1-A) * mu.hats$mu0
 
@@ -325,7 +345,7 @@ for (j in 1:1000){
 
 # save simulation data
 
-est.sim.rst.redo3.glm.all.interaction.SL <- data.frame(
+est.sim.rst.redo4.4 <- data.frame(
   seed = seed.list,
   n = rep(2000, 1000),
   psi.one.step.est = psi.one.step.est.list,
@@ -345,10 +365,11 @@ est.sim.rst.redo3.glm.all.interaction.SL <- data.frame(
   theta.se2 = theta.se2.list,
   theta.est2 = theta.est2.list
 )
-save(est.sim.rst.redo1.glm.mu, file="est.sim.rst.redo1.glm.mu.RData")
+save(est.sim.rst.redo4.3, file="est.sim.rst.redo4.3.RData")
 
 # analysis
-rst <- est.sim.rst.redo3.glm.all.interaction.SL
+rst <- est.sim.rst.redo4.4
+psi0
 # 1) bias
 # psi
 sqrt(n)*mean(rst$psi.plug.in - psi0)
@@ -375,11 +396,42 @@ sd(sqrt(n)*(rst$psi.est2 - psi0))
 mean(rst$psi.se2)
 
 sd(sqrt(n)*(rst$theta.plug.in - theta0))
-
 sd(sqrt(n)*(rst$theta.est - theta0))
+mean(rst$theta.se)
 
 sd(sqrt(n)*(rst$theta.plug.in2 - theta0))
-
 sd(sqrt(n)*(rst$theta.est2 - theta0))
+mean(rst$theta.se2)
 
 # mean(se)
+
+SL.gam.interaction <- function(Y, X, newX, family, obsWeights, id, deg.gam = 2, cts.num = 4, ...) {
+  # load required packages
+  # require('pkg')
+  # if(family$family == 'gaussian') {
+  #
+  # }
+  # if(family$family == 'binomial') {
+  #
+  # }
+  if(!require('gam')) {stop("SL.gam requires the gam package, but it isn't available")}
+  if("mgcv" %in% loadedNamespaces()) warning("mgcv and gam packages are both in use. You might see an error because both packages use the same function names.")
+  # create the formula for gam with a spline for each continuous variable
+  cts.x <- apply(X, 2, function(x) (length(unique(x)) > cts.num))
+  gam.model <- as.formula("Y ~ s(W1, 2) + s(W2, 2) + s(W1):A + s(W2):A + A*W3")
+  fit.gam <- gam::gam(gam.model, data = X, family = family, control = gam::gam.control(maxit = 50, bf.maxit = 50), weights = obsWeights)
+  if(packageVersion('gam') >= 1.15) {
+    pred <- gam::predict.Gam(fit.gam, newdata = newX, type = "response") # updated gam class in version 1.15
+  } else {
+    stop("This SL.gam wrapper requires gam version >= 1.15, please update the gam package with 'update.packages('gam')'")
+  }
+  # pred is the predicted responses for newX (on the scale of the outcome)
+  # pred <- numeric()
+  # fit returns all objects needed for predict.SL.template
+  fit <- list(object = fit.gam)
+  # declare class of fit for predict.SL.template
+  class(fit) <- 'SL.gam.interaction'
+  # return a list with pred and fit
+  out <- list(pred = pred, fit = fit)
+  return(out)
+}
